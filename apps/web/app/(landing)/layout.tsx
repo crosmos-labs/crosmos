2fa3ee603a1dee3ec1c0ffbdd6898aca3eee5a7a
Aditya Chaudhary <aditya.chaudhary0523@gmail.com>
2026-04-17 16:10:47 +0530

    chore(web): add landing page scrollbar styles

--- apps/web/app/(landing)/layout.tsx
+++ apps/web/app/(landing)/layout.tsx
@@ -1,6 +1,6 @@
 import { Footer } from "@/components/footer";
 import { Navbar } from "@/components/navbar";
-import "./landing.css";
+import "./style.css";
 
 export default function LandingLayout({
 	children,
