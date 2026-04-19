5b7876c8fd693a4f0d7cd4fca1402d5f491c13db
Aditya Chaudhary <aditya.chaudhary0523@gmail.com>
2026-04-19 18:23:00 +0530

    chore(app): revert back to base url envg

--- apps/app/lib/auth/redirect.ts
+++ apps/app/lib/auth/redirect.ts
@@ -1,7 +1,5 @@
 export function getRedirectUri() {
-    const baseUrl = `https://${process.env.VERCEL_URL} `;
-    console.log("VERCEL_URL: ", process.env.VERCEL_URL);
-    console.log("BASE_URL: ", process.env.NEXT_PUBLIC_BASE_URL);
+	const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 	if (baseUrl) return `${baseUrl}/auth/callback`;
 
 	return "http://localhost:3000/auth/callback";
